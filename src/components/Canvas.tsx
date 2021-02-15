0fa37aec5a5e6731ab48a726047153c91a104ca8
nicola <nsansoni97@gmail.com>
2021-02-15 15:18:51 +0100

    Fix

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -203,6 +203,8 @@ export default function Canvas({
     const onMouseLeave = () => {
         setPoints([]);
         if (dragging) {
+            setDragging(false);
+            setOldMouse(null);
             deleteSelected();
         }
     };
