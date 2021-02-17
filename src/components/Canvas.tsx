f259ef0984e76e7db81a0b28666ca8c1d91b28b3
nicola <nsansoni97@gmail.com>
2021-02-17 15:08:32 +0100

    Improve canvas look

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,7 +7,31 @@ import { distancePointToPoint, projectPointToSegment, findPointIndexInShape, vec
 const CANVAS_W = 1920;
 const CANVAS_H = 1080;
 const POINT_RADIUS = 5;
-const SNAP_DISTANCE = 50;
+const SNAP_DISTANCE = 25;
+
+const alpha = .25;
+
+const style1 = {
+    width: 2,
+    stroke: `rgb(250, 50, 50)`,
+    fill: `rgba(250, 50, 50, ${alpha})`,
+    selected: {
+        width: 2,
+        stroke: `rgb(255, 200, 50)`,
+        fill: `rgba(255, 200, 50, ${alpha})`,
+    },
+};
+
+const style2 = {
+    width: 2,
+    stroke: `rgb(50, 50, 250)`,
+    fill: `rgba(50, 50, 250, ${alpha})`,
+    selected: {
+        width: 2,
+        stroke: `rgb(50, 250, 250)`,
+        fill: `rgba(50, 250, 250, ${alpha})`,
+    },
+};
 
 export interface CanvasProps {
     img: CanvasImageSource | null;
@@ -61,14 +85,23 @@ export default function Canvas({
 
         // Finished Quads
         for (const quad of quads) {
+            let style;
+            if (quad.isCarWalkable) {
+                style = style1;
+            } else {
+                style = style2;
+            }
+            if (quad.isSelected)
+                style = style.selected;
+
+            context.lineWidth = style.width;
+            context.strokeStyle = style.stroke;
+            context.fillStyle = style.fill;
+
             if (quad.isSelected) {
-                context.strokeStyle = '#ff0';
-                context.fillStyle = '#0ff';
                 drawPath(context, quad.shape.map(p => vecSum(p, movement)), true);
             } else {
-                context.strokeStyle = '#f00';
-                context.fillStyle = '#00f';
-                drawPath(context, quad.shape, true);
+                drawPath(context, quad.shape);
             }
         }
 
@@ -94,7 +127,7 @@ export default function Canvas({
     const snapToShapes = (pos: Point, shapes: Shape[]) => {
         let [minDist, newPoint] = shapes.reduce(([dist, point]: [number, Point | null], s) => {
 
-            // find the most close point in each shape with his distance
+            // find the closest point in each shape with his distance
             let [d, p] = s.reduce(([dist, point]: [number, Point | null], p1, i, s) => {
 
                 const p2 = s[(i + 1) % s.length];
@@ -162,6 +195,7 @@ export default function Canvas({
             canvasCtx.closePath();
 
         canvasCtx.stroke();
+        canvasCtx.fill();
 
         if (f_drawPoints) {
             for (const p of points)
@@ -170,10 +204,17 @@ export default function Canvas({
     };
 
     const drawPoint = (canvasCtx: CanvasRenderingContext2D, point: Point) => {
+        const tmpFill = canvasCtx.fillStyle;
+        const tmpStroke = canvasCtx.strokeStyle;
+        canvasCtx.fillStyle = "#5f5";
+        canvasCtx.strokeStyle = "#000";
         canvasCtx.beginPath();
         canvasCtx.arc(point[0], point[1], POINT_RADIUS, 0, 2 * 3.15);
         canvasCtx.closePath();
         canvasCtx.fill();
+        canvasCtx.stroke();
+        canvasCtx.fillStyle = tmpFill;
+        canvasCtx.strokeStyle = tmpStroke;
     };
 
     const handleSelect = () => {
